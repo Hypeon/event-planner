da0187756d5c415375f223ca294ac9c67cf22a95
JK <kuba.kalicki@gmail.com>
2018-05-25 12:44:44 +0200

    Defined database connection

--- src/main/resources/database_creation.sql
+++ src/main/resources/database_creation.sql
@@ -1,17 +1,24 @@
-CREATE DATABASE  IF NOT EXISTS `event_planner`;
+
+
+CREATE USER IF NOT EXISTS 'user'@'localhost:3306/event_planner' IDENTIFIED BY 'user';
+GRANT ALL PRIVILEGES ON * . * TO 'user'@'localhost:3306/event_planner';
+	
+
+DROP DATABASE IF EXISTS`event_planner`;
+CREATE DATABASE `event_planner`;
 USE `event_planner`;
 
 CREATE TABLE `event` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `datetime_of_event` datetime DEFAULT CURRENT_TIMESTAMP,
-  `price` decimal(7,2) DEFAULT NULL,
+  `description` varchar(255) NOT NULL,
+  `datetime_of_event` datetime DEFAULT NULL,
+  `price` decimal(7,2) NOT NULL,
   -- CONSTRAINT contraint_name UNIQUE (id, email)
   -- `tag`
   `location_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (location_id) REFERENCES event(id)
+  FOREIGN KEY (location_id) REFERENCES location(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 
@@ -21,7 +28,7 @@ CREATE TABLE `location` (
   `name` varchar(60) NOT NULL,
   `address` varchar(80) NOT NULL,
   `lat` float(10,6) NOT NULL,
-  `lng` float(10,6) NOT NULL,
-  `type` varchar(30) NOT NULL,
+  `len` float(10,6) NOT NULL,
+  -- `type` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
